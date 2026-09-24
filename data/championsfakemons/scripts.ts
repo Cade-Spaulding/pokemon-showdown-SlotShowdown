64ac2b17bbd05e8729023b47d5b7dd0af9a99237
Cade Spaulding <51171344+Cade-Spaulding@users.noreply.github.com>
2026-09-24 11:24:36 -0500

    Update scripts.ts

--- data/championsfakemons/scripts.ts
+++ data/championsfakemons/scripts.ts
@@ -1,1 +1,12 @@
 
+import { Scripts as ChampionsScripts } from '../champions/scripts';
+
+export const Scripts: ModdedBattleScriptsData = {
+	inherit: 'champions',
+	gen: 9,
+
+	init() {
+		// Preserve Champions-specific initialization.
+		ChampionsScripts.init?.call(this);
+	},
+};
